29f4a6e269b5981bf5612513812a136b4e223bda
Sergey Grechin <grechin.sergey@gmail.com>
2021-12-19 09:03:44 +0300

    fixed raw->flat->raw conversion + test

--- sonic_pi_code_generator/lib/raw_ast.py
+++ sonic_pi_code_generator/lib/raw_ast.py
@@ -7,14 +7,28 @@ VALUE_EMPTY = 'EMPTY'
 
 
 class RawASTNode:
-    def __init__(self):
+    def __init__(self, raw_type: str = _UNDEFINED_TYPE, value: str = VALUE_EMPTY):
         self.children = []  # type: List[RawASTNode]
         self.parent = None  # type: Optional[RawASTNode]
-        self.type: str = _UNDEFINED_TYPE
-        self.value: str = VALUE_EMPTY
+        self.type: str = raw_type
+        self.value: str = value
+
+        # used when converting flat to raw
+        self.flat_ast_element: Optional[FlatASTElement] = None
+
+    def set_children(self, new_children):
+        # type: (List[RawASTNode])->None
+        self.children = new_children
+        for child in self.children:
+            child.parent = self
+
+    def add_child(self, new_child):
+        # type: (RawASTNode)->None
+        self.children.append(new_child)
+        new_child.parent = self
 
     def render_as_flat_ast_elements(self) -> List[FlatASTElement]:
-        # essentially it's "in-order depth first traversal"
+        # essentially it's an "in-order depth first traversal"
         # with putting "has children"
         # and "has right sibling" markers
         # as requested by https://arxiv.org/pdf/1711.09573.pdf
@@ -43,4 +57,56 @@ class RawASTNode:
                     return True
                 else:
                     return False
-        raise Exception('child not found among its parent\'s children')
+        raise Exception('new_node not found among its parent\'s children')
+
+
+class RawASTNodeFactory:
+    def construct_from_list_of_flat_elements(self, flat_elements: List[FlatASTElement]) -> RawASTNode:
+        last_created_node: Optional[RawASTNode] = None
+        root_node: Optional[RawASTNode] = None
+
+        for flat_element in flat_elements:
+            new_node = self._create_not_connected_node(flat_element)
+            self._connect_new_node(new_node, last_created_node)
+            if last_created_node is None:
+                root_node = new_node
+
+            last_created_node = new_node
+
+        if root_node is None:
+            raise Exception('root node not found')
+
+        return root_node
+
+    def _create_not_connected_node(self, flat_element: FlatASTElement) -> RawASTNode:
+        res = RawASTNode()
+        res.type = flat_element.type
+        res.value = flat_element.value
+        res.flat_ast_element = flat_element
+        return res
+
+    def _connect_new_node(self, new_node: RawASTNode, last_created_node: Optional[RawASTNode]):
+        if last_created_node is None:
+            return
+
+        if last_created_node.flat_ast_element.has_children:
+            parent = last_created_node
+        elif last_created_node.flat_ast_element.has_right_sibling:
+            parent = last_created_node.parent
+        else:
+            parent = self._find_closes_parent_with_right_sibling(last_created_node)
+
+        parent.add_child(new_node)
+
+    def _find_closes_parent_with_right_sibling(self, last_created_node: RawASTNode) -> RawASTNode:
+        cursor = last_created_node
+
+        while not cursor.flat_ast_element.has_right_sibling:
+            if cursor.parent is None:
+                raise Exception('reached the top and not found a parent to connect to!')
+
+            cursor = cursor.parent
+
+        # we have found the guy with a right sibling,
+        # so we need to return its parent
+        return cursor.parent
---
+++ tests/flat_tree_test.py
@@ -0,0 +1,67 @@
+import unittest
+from typing import List, Dict
+
+from sonic_pi_code_generator.lib.flat_ast import FlatASTElement
+from sonic_pi_code_generator.lib.raw_ast import RawASTNode, RawASTNodeFactory
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        """
+        the tree here is this one:
+        https://user-images.githubusercontent.com/21345604/146665558-db526b13-d729-4548-a44f-a5388252acf2.png
+
+        """
+        root_node = RawASTNode('t', '1')
+        node2 = RawASTNode('t', '2')
+        node3 = RawASTNode('t', '3')
+        node4 = RawASTNode('t', '4')
+        node5 = RawASTNode('t', '5')
+        node6 = RawASTNode('t', '6')
+        node7 = RawASTNode('t', '7')
+        node8 = RawASTNode('t', '8')
+
+        root_node.set_children([node2, node8, node5])
+        node2.set_children([node3, node7, node4])
+        node3.set_children([node6])
+
+        flat_elements = root_node.render_as_flat_ast_elements()
+        digest_before = self._create_flat_elements_digest(flat_elements)
+
+        self.assertEqual(digest_before,
+                         [{'t': 't', 'v': '1', 'c': '1', 'rs': '0'},
+                          {'t': 't', 'v': '2', 'c': '1', 'rs': '1'},
+                          {'t': 't', 'v': '3', 'c': '1', 'rs': '1'},
+                          {'t': 't', 'v': '6', 'c': '0', 'rs': '0'},
+                          {'t': 't', 'v': '7', 'c': '0', 'rs': '1'},
+                          {'t': 't', 'v': '4', 'c': '0', 'rs': '0'},
+                          {'t': 't', 'v': '8', 'c': '0', 'rs': '1'},
+                          {'t': 't', 'v': '5', 'c': '0', 'rs': '0'}])
+
+        self.assertEqual(len(flat_elements), 8)
+
+        factory = RawASTNodeFactory()
+
+        restored_raws = factory.construct_from_list_of_flat_elements(flat_elements)
+        flat_elements_after = restored_raws.render_as_flat_ast_elements()
+        digest_after = self._create_flat_elements_digest(flat_elements_after)
+
+        self.assertEqual(digest_after, digest_before)
+
+        pass
+
+    def _create_flat_elements_digest(self, flat_elements: List[FlatASTElement]) -> List[Dict[str, str]]:
+        res = []
+        for element in flat_elements:
+            res.append({
+                "t": element.type,
+                "v": element.value,
+                "c": "1" if element.has_children else "0",
+                "rs": "1" if element.has_right_sibling else "0"
+            })
+
+        return res
+
+
+if __name__ == '__main__':
+    unittest.main()
